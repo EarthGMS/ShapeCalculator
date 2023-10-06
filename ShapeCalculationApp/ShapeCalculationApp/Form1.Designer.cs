8595ddbb118145e59677e0a5bc463345668cf289
Ume <79046153+EarthGMS@users.noreply.github.com>
2023-10-06 19:35:58 +0700

    first push

---
+++ ShapeCalculationApp/ShapeCalculationApp/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace ShapeCalculationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DescText = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Square1Button = new System.Windows.Forms.Button();
+            this.Circle1Button = new System.Windows.Forms.Button();
+            this.Shape1 = new System.Windows.Forms.Label();
+            this.Shape2Text = new System.Windows.Forms.Label();
+            this.Circle2Button = new System.Windows.Forms.Button();
+            this.Triangle2Button = new System.Windows.Forms.Button();
+            this.Square2Button = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.Triangle1Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DescText
+            // 
+            this.DescText.AutoSize = true;
+            this.DescText.Location = new System.Drawing.Point(14, 19);
+            this.DescText.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.DescText.Name = "DescText";
+            this.DescText.Size = new System.Drawing.Size(117, 13);
+            this.DescText.TabIndex = 0;
+            this.DescText.Text = "SHAPE CALCULATOR";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(27, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 288);
+            this.panel1.TabIndex = 2;
+            // 
+            // Square1Button
+            // 
+            this.Square1Button.Location = new System.Drawing.Point(27, 66);
+            this.Square1Button.Name = "Square1Button";
+            this.Square1Button.Size = new System.Drawing.Size(74, 28);
+            this.Square1Button.TabIndex = 3;
+            this.Square1Button.Text = "SQUARE";
+            this.Square1Button.UseVisualStyleBackColor = true;
+            // 
+            // Circle1Button
+            // 
+            this.Circle1Button.Location = new System.Drawing.Point(187, 66);
+            this.Circle1Button.Name = "Circle1Button";
+            this.Circle1Button.Size = new System.Drawing.Size(74, 28);
+            this.Circle1Button.TabIndex = 5;
+            this.Circle1Button.Text = "CIRCLE";
+            this.Circle1Button.UseVisualStyleBackColor = true;
+            // 
+            // Shape1
+            // 
+            this.Shape1.AutoSize = true;
+            this.Shape1.Location = new System.Drawing.Point(153, 39);
+            this.Shape1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.Shape1.Name = "Shape1";
+            this.Shape1.Size = new System.Drawing.Size(52, 13);
+            this.Shape1.TabIndex = 6;
+            this.Shape1.Text = "SHAPE 1";
+            // 
+            // Shape2Text
+            // 
+            this.Shape2Text.AutoSize = true;
+            this.Shape2Text.Location = new System.Drawing.Point(539, 39);
+            this.Shape2Text.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.Shape2Text.Name = "Shape2Text";
+            this.Shape2Text.Size = new System.Drawing.Size(52, 13);
+            this.Shape2Text.TabIndex = 11;
+            this.Shape2Text.Text = "SHAPE 2";
+            // 
+            // Circle2Button
+            // 
+            this.Circle2Button.Location = new System.Drawing.Point(573, 66);
+            this.Circle2Button.Name = "Circle2Button";
+            this.Circle2Button.Size = new System.Drawing.Size(74, 28);
+            this.Circle2Button.TabIndex = 10;
+            this.Circle2Button.Text = "CIRCLE";
+            this.Circle2Button.UseVisualStyleBackColor = true;
+            // 
+            // Triangle2Button
+            // 
+            this.Triangle2Button.Location = new System.Drawing.Point(493, 66);
+            this.Triangle2Button.Name = "Triangle2Button";
+            this.Triangle2Button.Size = new System.Drawing.Size(74, 28);
+            this.Triangle2Button.TabIndex = 9;
+            this.Triangle2Button.Text = "TRIANGLE";
+            this.Triangle2Button.UseVisualStyleBackColor = true;
+            // 
+            // Square2Button
+            // 
+            this.Square2Button.Location = new System.Drawing.Point(413, 66);
+            this.Square2Button.Name = "Square2Button";
+            this.Square2Button.Size = new System.Drawing.Size(74, 28);
+            this.Square2Button.TabIndex = 8;
+            this.Square2Button.Text = "SQUARE";
+            this.Square2Button.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(413, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(319, 288);
+            this.panel2.TabIndex = 7;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(280, 406);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(207, 32);
+            this.CalculateButton.TabIndex = 12;
+            this.CalculateButton.Text = "CALCULATE!";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            // 
+            // Triangle1Button
+            // 
+            this.Triangle1Button.Location = new System.Drawing.Point(107, 66);
+            this.Triangle1Button.Name = "Triangle1Button";
+            this.Triangle1Button.Size = new System.Drawing.Size(74, 28);
+            this.Triangle1Button.TabIndex = 13;
+            this.Triangle1Button.Text = "TRIANGLE";
+            this.Triangle1Button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Triangle1Button);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.Shape2Text);
+            this.Controls.Add(this.Shape1);
+            this.Controls.Add(this.Circle2Button);
+            this.Controls.Add(this.Triangle2Button);
+            this.Controls.Add(this.Circle1Button);
+            this.Controls.Add(this.Square2Button);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Square1Button);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DescText);
+            this.Name = "Form1";
+            this.Text = "Shape Calculator App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DescText;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Square1Button;
+        private System.Windows.Forms.Button Circle1Button;
+        private System.Windows.Forms.Label Shape1;
+        private System.Windows.Forms.Label Shape2Text;
+        private System.Windows.Forms.Button Circle2Button;
+        private System.Windows.Forms.Button Triangle2Button;
+        private System.Windows.Forms.Button Square2Button;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button Triangle1Button;
+    }
+}
+
